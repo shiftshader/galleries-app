dc902c4bf585f2d41c00d46fb60a641ccbc36624
Nikola Stankovic <nikolastankovic1990@gmail.com>
2022-04-25 23:34:26 +0200

    need lodsa fixing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { store } from './store';
+import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
